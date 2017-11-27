7e7b8eb0e06d46f3f4e53a6466693e4c15217fed
LINQ-MATEBOOK\Linq <yzmslinq@gmail.com>
2017-11-27 16:00:04 +0800

    创建例2

---
+++ webrobot.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_script import Server, Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+import sys
+sys.path.insert(0, "../")
+
+import aiml
+k = aiml.Kernel()
+k.learn("cn-startup.xml")
+k.respond("load aiml cn")
+k.respond("start")
+                         
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
+server = Server(host="0.0.0.0", port=8082, threaded=True)
+manager.add_command("runserver", server)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):                 
+    name = StringField('请开始交谈：', validators=[Required()])
+    submit = SubmitField('提交')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = ''
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=k.respond(name))
+
+if __name__ == '__main__':
+    manager.run()
